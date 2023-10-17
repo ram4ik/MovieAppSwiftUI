fef255c465c65359b02854930e5a08d3afd45d42
ram4ik <ram4ik@hotmail.com>
2023-10-16 23:21:18 -0700

    Initial Commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by admin on 10/16/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
